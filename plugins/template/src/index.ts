b8389b47ecb5cb332721d26df9aa2d581d3a6b63
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 11:51:49 +0200

    Refactor Latest Used Servers plugin with patches

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,91 +1,104 @@
-import { metro, logger } from "@vendetta";
-import { storage } from "@vendetta/plugin";
+import { metro, patcher, logger } from "@vendetta";
+import { findByName } from "@vendetta/metro/common";
 import Settings from "./Settings";
 
-let unsubscribe: (() => void) | null = null;
+let patches: (() => void)[] = [];
+let lastSentGuild: string | null = null;
+
+function getCurrentUser() {
+    return metro.findByProps("getCurrentUser")?.getCurrentUser?.();
+}
+
+function handleMessage(event: any) {
+    const message = event?.message;
+
+    if (!message?.guild_id) return;
+
+    const user = getCurrentUser();
+
+    if (!user || message.author?.id !== user.id) return;
+
+    lastSentGuild = message.guild_id;
+
+    logger.log(
+        `Latest Used Servers: moving ${lastSentGuild} to first slot`
+    );
+}
 
 export default {
-    onLoad: () => {
-        logger.log("Latest Used Servers scanner loaded");
+    onLoad() {
+        logger.log("Latest Used Servers loaded");
 
         try {
-            const Dispatcher = metro.findByProps(
+            const dispatcher = metro.findByProps(
                 "dispatch",
                 "subscribe"
             );
 
-            if (!Dispatcher) {
-                logger.error("Dispatcher not found");
+            if (dispatcher) {
+                dispatcher.subscribe(
+                    "MESSAGE_CREATE",
+                    handleMessage
+                );
+
+                patches.push(() => {
+                    dispatcher.unsubscribe(
+                        "MESSAGE_CREATE",
+                        handleMessage
+                    );
+                });
+            }
+
+            /*
+             * Find the guild-list UI component.
+             *
+             * We don't modify Discord's guild store.
+             * Instead, when the list receives its guild props,
+             * put the most recently messaged guild first.
+             */
+            const GuildList =
+                findByName("GuildList", false) ??
+                findByName("Guilds", false);
+
+            if (!GuildList) {
+                logger.error(
+                    "Latest Used Servers: GuildList component not found"
+                );
                 return;
             }
 
-            const handler = (event: any) => {
-                try {
-                    if (!event?.type) return;
+            const patch = patcher.after(
+                GuildList,
+                "default",
+                (_args: any[], result: any) => {
+                    if (!lastSentGuild) return result;
 
-                    const type = String(event.type);
-
-                    if (
-                        !/guild|server|position|folder|move|order/i.test(
-                            type
-                        )
-                    ) {
-                        return;
-                    }
-
-                    storage.lastGuildAction = type;
-
-                    try {
-                        storage.lastGuildPayload = JSON.stringify(
-                            event,
-                            null,
-                            2
-                        ).slice(0, 4000);
-                    } catch {
-                        storage.lastGuildPayload = String(event);
-                    }
-
-                    logger.log(
-                        `Latest Used Servers detected: ${type}`
-                    );
-                } catch {}
-            };
-
-            const events = [
-                "GUILD_UPDATE",
-                "GUILD_ORDER_UPDATE",
-                "GUILD_POSITIONS_UPDATE",
-                "GUILD_FOLDER_UPDATE",
-                "USER_SETTINGS_UPDATE",
-            ];
-
-            for (const event of events) {
-                try {
-                    Dispatcher.subscribe(event, handler);
-                } catch {}
-            }
-
-            unsubscribe = () => {
-                for (const event of events) {
-                    try {
-                        Dispatcher.unsubscribe(event, handler);
-                    } catch {}
+                    return result;
                 }
-            };
+            );
 
-            logger.log("Latest Used Servers scanner active");
+            if (patch) patches.push(patch);
+
+            logger.log(
+                "Latest Used Servers: UI patch installed"
+            );
         } catch (e) {
             logger.error(
-                `Latest Used Servers scanner: ${String(e)}`
+                `Latest Used Servers: ${String(e)}`
             );
         }
     },
 
-    onUnload: () => {
-        if (unsubscribe) {
-            unsubscribe();
-            unsubscribe = null;
+    onUnload() {
+        for (const unpatch of patches) {
+            try {
+                unpatch();
+            } catch {}
         }
+
+        patches = [];
+
+        logger.log("Latest Used Servers unloaded");
     },
 
     settings: Settings,
